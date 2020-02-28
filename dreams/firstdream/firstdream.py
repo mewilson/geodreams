98c477c3d6fe2a09795eff6d01e122b8a654ef3b
Mel <mwilsonreyes@gmail.com>
2020-02-27 23:52:55 -0600

    added rest of dreams and reorganized file structure.

--- dreams/firstdream/firstdream.py
+++ dreams/firstdream/firstdream.py
@@ -6,16 +6,13 @@ def wind_mill(center_x, center_y, color = "black"):
     t.setpos(center_x, center_y)
     t.pd()
     
-    # t.pencolor(color)
-    t.begin_fill()
+    t.pencolor(color)
 
     for i in range(4):
         t.fd(30)
         t.lt(90)
         t.fd(40)
         t.setpos(center_x, center_y)
-
-    t.end_fill()
 
 def row_left(function, center_x, center_y, first_value, second_value, colors, n):
 
@@ -56,24 +53,11 @@ def main():
     # coord_center(wind_mill, 30, 40)
 
     colors = ["red", "orange", "green", "blue", "purple"]
-    colors = ["black", "black", "black", "black", "black"]
-
-    recursion_const = 4
-    
-    
-
-    row(wind_mill, 0, 0, 60, 40, colors, recursion_const)
-
-    
-    for i in range(1, 4):
-        row(wind_mill, i * 40, i * -60, 60, 40, colors, recursion_const) # Downward
-        row(wind_mill, i * -40, i * 60, 60, 40, colors, recursion_const) # Upward
-
+    row(wind_mill, 0, 0, 60, 40, colors, 4)
+    row(wind_mill, -40, 60, 60, 40, colors, 4)
     
     #dynamic_center(60, 40, 60, 40)
     #wind_mill(60, 40)
-
-    t.getscreen().getcanvas().postscript(file = "firstdream2.eps")
     
     
 
